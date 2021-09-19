6c9a6a62a3d55323de8f38d28b929525d2b5b7d5
Gupta-Anubhav12 <anubhavgupta2260@gmail.com>
2021-09-19 12:49:48 +0530

    Update Readme

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -129,7 +129,7 @@ export default function SearchBar() {
             closeMenuOnSelect={false}
             components={{ ClearIndicator, MultiValueRemove }}
             styles={customStyles}
-            // defaultValue={[colourOptions[4], colourOptions[5]]}
+            // defaultValue={[...options[]]}
             isMulti
             onChange={setSelected}
             options={options}
